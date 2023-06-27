31922a43c7a9d22d3bd03f2a35f7f4ef17960177
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-06-27 14:44:13 +0700

    Update portal_add_user.py

--- portal_add_user.py
+++ portal_add_user.py
@@ -4,60 +4,60 @@ import json
 import mysql.connector
 import subprocess
 
-def connect_to_database_and_run_curl():
-    try:
-        # Menghubungkan ke database
-        cnx = mysql.connector.connect(user='meliska', password='meliska', host='127.0.0.1', database='repository')
-        cursor = cnx.cursor()
+# def connect_to_database_and_run_curl():
+#     try:
+#         # Menghubungkan ke database
+#         cnx = mysql.connector.connect(user='meliska', password='meliska', host='127.0.0.1', database='repository')
+#         cursor = cnx.cursor()
 
-        # Mendapatkan data username dan password dari database
-        cursor.execute("SELECT nim, password FROM users")
-        users = cursor.fetchall()
+#         # Mendapatkan data username dan password dari database
+#         cursor.execute("SELECT nim, password FROM users")
+#         users = cursor.fetchall()
 
-        # Menjalankan perintah curl untuk setiap user
-        for user in users:
-            nim, password = user
+#         # Menjalankan perintah curl untuk setiap user
+#         for user in users:
+#             nim, password = user
 
-            url = 'http://10.0.0.20:8080/api/add_unix_user/'
-            payload = {'userPass': password, 'userId': nim}
+#             url = 'http://10.0.0.20:8080/api/add_unix_user/'
+#             payload = {'userPass': password, 'userId': nim}
 
-            # Convert payload to JSON string
-            payload_json = json.dumps(payload)
+#             # Convert payload to JSON string
+#             payload_json = json.dumps(payload)
 
-            # Construct the curl command
-            curl_command = f"curl -X POST -d '{payload_json}' {url}"
+#             # Construct the curl command
+#             curl_command = f"curl -X POST -d '{payload_json}' {url}"
 
-            try:
-                # Execute curl command using subprocess
-                process = subprocess.Popen(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-                output, error = process.communicate()
+#             try:
+#                 # Execute curl command using subprocess
+#                 process = subprocess.Popen(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+#                 output, error = process.communicate()
 
-                if process.returncode != 0:
-                    raise subprocess.CalledProcessError(process.returncode, curl_command, output=output, stderr=error)
+#                 if process.returncode != 0:
+#                     raise subprocess.CalledProcessError(process.returncode, curl_command, output=output, stderr=error)
 
-                # Parse the response
-                data = json.loads(output)
+#                 # Parse the response
+#                 data = json.loads(output)
                 
-                if data is not None:
-                    # Menggunakan pesan respons untuk tindakan lanjutan
-                    message = f"Received response from curl for user '{nim}': {data}"
-                    print(message)
-                    # Lakukan tindakan lain yang diperlukan berdasarkan pesan respons
-                else:
-                    print(f"Failed to get curl response for user '{nim}'.")
-            except subprocess.CalledProcessError as e:
-                print(f"Error executing curl command: {e.output}")
-            except Exception as e:
-                print(f"An error occurred: {e}")
+#                 if data is not None:
+#                     # Menggunakan pesan respons untuk tindakan lanjutan
+#                     message = f"Received response from curl for user '{nim}': {data}"
+#                     print(message)
+#                     # Lakukan tindakan lain yang diperlukan berdasarkan pesan respons
+#                 else:
+#                     print(f"Failed to get curl response for user '{nim}'.")
+#             except subprocess.CalledProcessError as e:
+#                 print(f"Error executing curl command: {e.output}")
+#             except Exception as e:
+#                 print(f"An error occurred: {e}")
 
-        # Menutup koneksi ke database
-        cursor.close()
-        cnx.close()
-    except mysql.connector.Error as err:
-        print(f"An error occurred while connecting to the database: {err}")
+#         # Menutup koneksi ke database
+#         cursor.close()
+#         cnx.close()
+#     except mysql.connector.Error as err:
+#         print(f"An error occurred while connecting to the database: {err}")
 
-# Panggil fungsi untuk terhubung ke database dan menjalankan perintah curl
-connect_to_database_and_run_curl()
+# # Panggil fungsi untuk terhubung ke database dan menjalankan perintah curl
+# connect_to_database_and_run_curl()
 
 
 
@@ -113,15 +113,15 @@ connect_to_database_and_run_curl()
 # connect_to_database_and_run_curl()
 
 
-# def create_unix_user(userPass, userId):
-#     url = 'http://10.0.0.20:8080/api/add_unix_user/'
-#     payload = {'userPass': userPass, 'userId': userId}
-#     response = requests.post(url, data=payload)
-#     data = response.json()
-#     return data
+def create_unix_user(userPass, userId):
+    url = 'http://10.0.0.20:8080/api/add_unix_user/'
+    payload = {'userPass': userPass, 'userId': userId}
+    response = requests.post(url, data=payload)
+    data = response.json()
+    return data
 
-# userPass = "password2"
-# userId = "user1234"
-# result = create_unix_user(userPass, userId)
-# print(result)
+userPass = "password2"
+userId = "user1234"
+result = create_unix_user(userPass, userId)
+print(result)
 
