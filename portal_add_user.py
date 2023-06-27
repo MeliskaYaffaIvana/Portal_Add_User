13b95e1225e18add5295cf5ac2eb65dfcdf532dc
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-06-27 12:28:26 +0700

    Update portal_add_user.py
    connect db

--- portal_add_user.py
+++ portal_add_user.py
@@ -1,13 +1,74 @@
-import requests
+import json
+import mysql.connector
+import subprocess
 
-def create_unix_user(userPass, userId):
-    url = 'http://127.0.0.1:8080/api/add_unix_user/'
-    payload = {'userPass': userPass, 'userId': userId}
-    response = requests.post(url, data=payload)
-    data = response.json()
-    return data
+def connect_to_database_and_run_curl():
+    try:
+        # Menghubungkan ke database
+        cnx = mysql.connector.connect(user='meliska', password='meliska', host='127.0.0.1', database='repository')
+        cursor = cnx.cursor()
 
-userPass = "password"
-userId = "user123"
-result = create_unix_user(userPass, userId)
-print(result)
+        # Mendapatkan data username dan password dari database
+        cursor.execute("SELECT nim, password FROM users")
+        users = cursor.fetchall()
+
+        # Menjalankan perintah curl untuk setiap user
+        for user in users:
+            nim, password = user
+
+            url = 'http://10.0.0.20:8080/api/add_unix_user/'
+            payload = {'userPass': password, 'userId': nim}
+
+            # Convert payload to JSON string
+            payload_json = json.dumps(payload)
+
+            # Construct the curl command
+            curl_command = f"curl -X POST -d '{payload_json}' {url}"
+
+            try:
+                # Execute curl command using subprocess
+                process = subprocess.Popen(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                output, error = process.communicate()
+
+                if process.returncode != 0:
+                    raise subprocess.CalledProcessError(process.returncode, curl_command, output=output, stderr=error)
+
+                # Parse the response
+                data = json.loads(output)
+                
+                if data is not None:
+                    # Menggunakan pesan respons untuk tindakan lanjutan
+                    message = f"Received response from curl for user '{nim}': {data}"
+                    print(message)
+                    # Lakukan tindakan lain yang diperlukan berdasarkan pesan respons
+                else:
+                    print(f"Failed to get curl response for user '{nim}'.")
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing curl command: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+        # Menutup koneksi ke database
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        print(f"An error occurred while connecting to the database: {err}")
+
+# Panggil fungsi untuk terhubung ke database dan menjalankan perintah curl
+connect_to_database_and_run_curl()
+
+
+
+# import requests
+
+# def create_unix_user(userPass, userId):
+#     url = 'http://127.0.0.1:8080/api/add_unix_user/'
+#     payload = {'userPass': userPass, 'userId': userId}
+#     response = requests.post(url, data=payload)
+#     data = response.json()
+#     return data
+
+# userPass = "password"
+# userId = "user123"
+# result = create_unix_user(userPass, userId)
+# print(result)
