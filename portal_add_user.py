40697a7303e09d07f74de012a3b00d8a168b3d3b
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-05 12:58:36 +0700

    Update portal_add_user.py

--- portal_add_user.py
+++ portal_add_user.py
@@ -1,17 +1,14 @@
-
-
 import requests
-
 import subprocess
 import mysql.connector
 import shlex
 
 # Koneksi ke database MySQL
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="meliska",
-  password="meliska",
-  database="repository"
+    host="127.0.0.1",
+    user="meliska",
+    password="meliska",
+    database="repository"
 )
 
 # Membuat kursor untuk mengeksekusi perintah SQL
@@ -21,10 +18,6 @@ cursor = mydb.cursor()
 query = "SELECT id, nim FROM users"
 cursor.execute(query)
 results = cursor.fetchall()
-
-# Menutup kursor dan koneksi ke database
-cursor.close()
-mydb.close()
 
 # Mengirim data ke server menggunakan curl
 url = 'http://10.0.0.20:8181/api/add_unix_user/'
@@ -43,3 +36,7 @@ for id, nim in results:
         print("Data berhasil diperbarui")
     except subprocess.CalledProcessError as e:
         print(f"Terjadi kesalahan saat mengirim data: {e}")
+
+# Menutup kursor dan koneksi ke database
+cursor.close()
+mydb.close()
